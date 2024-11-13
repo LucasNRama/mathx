05dba09f79a57b77ad08659a1f2e2e4ab427f50a
LucasNRama <lucasrama2002@gmail.com>
2024-11-13 14:45:40 -0300

    002

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,17 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function home()
+    public function home(): View
     {
-        echo 'apresentar a página inicial';
+        return view('home');   
     }
     
     public function generateExercises(Request $request)
     {
-        echo 'gerar exerícios';
+        // form validation
+        $request->validate([
+            'check_sum' => 'required_without_all:check_subtraction, check_multiplication, check_division',
+            'check_subtraction' => 'required_without_all:check_sum, check_multiplication, check_division',
+            'check_multiplication' => 'required_without_all:check_subtraction, check_sum, check_division',
+            'check_division' => 'required_without_all:check_subtraction, check_multiplication, check_sum',
+            'number_one' => 'required|integer|min:0|max:999',
+            'number_two' => 'required|integer|min:0|max:999',
+            'number_exercises' => 'required|integer|min:5|max:50',
+        ]);
+
+        dd($request->all());
     }
 
     public function printExercises()
